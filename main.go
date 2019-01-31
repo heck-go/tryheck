9fcd49e87e1425732cc3ecfe4a81b3e9544fe07a
tejainece <tejainece@gmail.com>
2019-01-31 22:38:03 +0100

    Query and Path parameters

--- main.go
+++ main.go
@@ -6,15 +6,33 @@ import (
 )
 
 func main() {
-	server := heck.NewServer(":10000")
-	server.GetFor("/api/name", func(ctx *heck.Context) {
-		ctx.Response = heck.Response{
+	server := heck.NewServer(":15000")
+	server.GetFor("/api/hello", func(ctx *heck.Context) {
+		ctx.Response = &heck.Response{
 			StatusCode: 200,
-			Value:      "Teja",
+			Value:      "Hello stranger!",
 		}
 	}, nil).Before(func(ctx *heck.Context) {
 		fmt.Println("Received route: ", ctx.Method())
+		ctx.Before(func(ctx *heck.Context) {
+			fmt.Println("Path: ", ctx.Path())
+		})
 	})
+	server.GetFor("/api/hello/:name", func(ctx *heck.Context) {
+		name, _ := ctx.PathParams.Get("name")
+		ctx.Response = &heck.Response{
+			StatusCode: 200,
+			Value:      "Hello " + name + "!",
+		}
+	}, nil)
+	server.GetFor("/api/math/add", func(ctx *heck.Context) {
+		a, _, _ := ctx.Query.Int("a")
+		b, _, _ := ctx.Query.Int("b")
+		ctx.Response = &heck.Response{
+			StatusCode: 200,
+			Value:      a + b,
+		}
+	}, nil)
 	if err := server.Start(); err != nil {
 		panic(err)
 	}
